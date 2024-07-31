6747a7367826696454ba5b9a7392a07607a076c4
Tiago Garbi <tgbmateriais@gmail.com>
2024-07-31 18:05:25 -0300

    feature: include components

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
 
-const Document = () => {
-  return (
-    <div className='flex flex-col'>
-    </div>
-  )
-}
+import CollaborativeRoom from "@/components/CollaborativeRoom";
+import { getDocument } from "@/lib/actions/document/read";
+import { getClerkUsers } from "@/lib/actions/user/getClerckUser";
 
-export default Document
+const Document = async ({ params: { id } }: SearchParamProps) => {
+    const clerkUser = await currentUser();
+    if (!clerkUser) redirect("/sign-in");
+
+    const room = await getDocument({
+        roomId: id,
+        userId: clerkUser.emailAddresses[0].emailAddress,
+    });
+
+    if (!room) redirect("/");
+
+    const userIds = Object.keys(room.usersAccesses);
+    const users = await getClerkUsers({ userIds });
+
+    const usersData = users.map((user: User) => ({
+        ...user,
+        userType: room.usersAccesses[user.email]?.includes("room:write")
+            ? "editor"
+            : "viewer",
+    }));
+
+    const currentUserType = room.usersAccesses[
+        clerkUser.emailAddresses[0].emailAddress
+    ]?.includes("room:write")
+        ? "editor"
+        : "viewer";
+
+    return (
+        <main className="flex w-full flex-col items-center">
+            <CollaborativeRoom
+                roomId={id}
+                roomMetadata={room.metadata}
+                users={usersData}
+                currentUserType={currentUserType}
+            />
+        </main>
+    );
+};
+
+export default Document;
